b83d726abc8250a46c707c9aaf050104fef374d8
NayutaY <nayutay@naver.com>
2018-07-09 02:25:23 -0500

    first commit

---
+++ 8ball.js
@@ -0,0 +1,37 @@
+const Discord = require("discord.js");
+
+const TOKEN = "NDY1NzYzNDI2NTY0ODMzMjgw.DiSPiA.4c_IfFYH5TE8KHzOqqw7dQczxsI"
+const PREFIX = "!"
+
+var fortunes = [ 
+    "Yes",
+    "No",
+    "Maybe",
+    "Never"
+];
+
+var bot = new Discord.Client();
+
+bot.on("ready", function() {
+    console.log("8Ball listo!");
+});
+
+bot.on("message", function(message) {
+    if (message.author.equals(bot.user)) return;
+
+    if (!message.content.startsWith(PREFIX)) return;
+    
+    var args = message.content.substring(PREFIX.length).split(" ");
+
+    switch (args[0].toLowerCase()) {
+        case "Hi":
+            message.channel.sendMessage("Hi,there!");
+            break;
+        case "8ball":
+            if (args[1]) message.channel.sendMessage(fortunes[Math.floor(Math.random() * fortunes.length)]);          
+            else message.channel.sendMessage("Can't read that");
+            break;
+    }
+});
+
+bot.login(TOKEN);
